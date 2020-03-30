2a0a53d96fed4be8bcce54c31742afd2aad1b68e
Signoraelle <alicecasadei@icloud.com>
2020-03-30 11:54:15 +0200

    quest oo1

--- the-adventurer-s-guide/src/App.js
+++ the-adventurer-s-guide/src/App.js
@@ -1,26 +1,18 @@
 import React from "react";
-
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignIn from "./components/Products/SingIn";
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Rout path="/singin" component={SignIn} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
